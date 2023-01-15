f79e574045c17e4d289f76b19a548702b2932255
Homam Jabir <homam.jabir@tietoevry.com>
2023-01-15 14:23:10 +0100

    initial commit

---
+++ src/main/java/com/mystore/ecommerce/model/User.java
@@ -0,0 +1,40 @@
+package com.mystore.ecommerce.model;
+
+public class User {
+    private String username;
+    private String email;
+    private String customerId;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
